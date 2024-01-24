0af25c52acbe989997c8dbaf3581500d7665f31b
anisimov <m0534171@gmail.com>
2024-01-24 15:15:45 +0300

    restart

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 
 setup(
-    name='MyLibrary',                    # package name
-    version='0.1',                          # version
+    name='TestLibrary',                    # package name
+    version='0.2',                          # version
     description='Package Description will be later',      # short description
     url='https://github.com/AnisimovMike/TestLibrary',               # package URL
     install_requires=[],                    # list of packages this package depends
                                             # on.
-    packages=['MyLibrary'],              # List of module names that installing
+    packages=['TestLibrary'],              # List of module names that installing
                                             # this package will provide.
 )
