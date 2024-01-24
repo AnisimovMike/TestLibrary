cdd5bfad247b53ab33c2bd286367c367ff205e3c
anisimov <m0534171@gmail.com>
2024-01-24 14:14:58 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='MyLibrary',                    # package name
+    version='0.1',                          # version
+    description='Package Description will be later',      # short description
+    url='',               # package URL
+    install_requires=[],                    # list of packages this package depends
+                                            # on.
+    packages=['MyLibrary'],              # List of module names that installing
+                                            # this package will provide.
+)
